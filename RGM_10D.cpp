00f1532ded1ce8702101f0ad1b8bc248286f2143
Weipeng Huang <weipeng.huang@insight-centre.org>
2024-11-26 09:29:16 +0800

    code

---
+++ RGM_10D.cpp
@@ -0,0 +1,38 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericMatrix association(IntegerVector Z){
+  int n = Z.size();
+  NumericMatrix S(n, n);
+  for (int i = 0; i < n; i ++){
+    for (int j = 0; j < n; j ++){
+      if (Z(i) == Z(j)){
+        S(i, j) = 1;
+      }
+      else
+        {
+          S(i, j) = 0;
+        }
+    }
+  }
+  return(S);
+}
+
+// [[Rcpp::export]]
+NumericMatrix coclustering(NumericVector gamma, double tol){
+  int n = gamma.size();
+  NumericMatrix S(n, n);
+  for (int i = 0; i < n; i ++){
+    for (int j = 0; j < n; j ++){
+      if ((gamma(i) - gamma(j) < tol) && (gamma(j) - gamma(i) < tol)){
+        S(i, j) = 1;
+      }
+      else
+      {
+        S(i, j) = 0;
+      }
+    }
+  }
+  return(S);
+}
