6c29cedfabb4efe2e5d4216cac64606b7f87984f
Weipeng <weipeng.huang@outlook.com>
2025-03-11 22:24:47 +0800

    new file

---
+++ WassersteinBayesRepulsiveGMM/src/mcmc/kernel_prior.jl
@@ -0,0 +1,98 @@
+mutable struct ScaleMvNormal
+    τ::Float64
+    mvn::MvNormal
+end 
+
+
+mutable struct EigBoundedIW
+    l_σ2::Float64
+    u_σ2::Float64
+    iw::InverseWishart 
+end 
+
+
+mutable struct KernelPrior
+    dim::Int
+    smvn::ScaleMvNormal
+    biw::EigBoundedIW
+end 
+
+
+EigBoundedIW(l_σ2, u_σ2, ν₀, Φ₀) = EigBoundedIW(
+    l_σ2, u_σ2, InverseWishart(ν₀, round.(Φ₀, digits=10) |> PDMat))
+
+
+KernelPrior(τ, μ₀, Σ₀, l_σ2, u_σ2, ν₀, Φ₀) = KernelPrior(
+    size(μ₀, 1), 
+    ScaleMvNormal(τ, MvNormal(μ₀, Σ₀)), 
+    EigBoundedIW(l_σ2, u_σ2, ν₀, round.(Φ₀, digits=10) |> PDMat)) 
+
+
+@inline function rand(biw::EigBoundedIW; max_cnt=2000)  
+    dim = size(biw.iw.Ψ, 1) 
+    
+    Σ = zeros(dim, dim)
+    eig_v_Σ = nothing 
+
+    l_σ2, u_σ2 = biw.l_σ2, biw.u_σ2
+    @inbounds for c = 1:max_cnt 
+        Σ .= rand(biw.iw) 
+        Σ .= round.(Σ, digits=10)    
+        eig_v_Σ = eigvals(Σ)
+        
+        (first(eig_v_Σ) > l_σ2 && last(eig_v_Σ) < u_σ2) && break  
+        
+        c < max_cnt ||
+            throw("Sampling from the prior takes too long. 
+                   Check if the bounds are set properly")
+    end  
+    return Σ
+end 
+
+function rand(prior::KernelPrior; max_cnt=2000)  
+    Σ = rand(prior.biw; max_cnt=max_cnt)
+    μ = rand(prior.smvn.mvn)   
+    return μ, Σ
+end 
+
+
+@inline function post_sample_gauss!(
+    X, Mu, Sig, C, k_prior::KernelPrior)
+
+    K = size(Mu, 2)
+    @inbounds for k in 1:K
+        Xₖ = X[:, C .== k] 
+        n = size(Xₖ, 2)  
+        
+        μ, Σ = n == 0 ? 
+            rand(k_prior) : 
+            post_sample_gauss(Xₖ, Mu[:, :, k], Sig[:, :, k], k_prior)
+        
+        Mu[:, k] .= μ[:] 
+        Sig[:, :, k] .= Σ[:, :]
+    end 
+end 
+
+
+@inline function post_sample_gauss(X, μ, Σ, k_prior::KernelPrior)
+    # An inner function for computing the covariance
+    # μ is currently not in use
+    cov_(X) = cov(X; dims=2, corrected=false)
+
+    dim, n = size(X)
+    τ = k_prior.smvn.τ
+  
+    x̄ = mean(X; dims=2)
+
+    νₙ = k_prior.biw.iw.df + n 
+    Ψₙ = Matrix(k_prior.biw.iw.Ψ) + cov_(X) * n  
+    Ψₙ = round.(Ψₙ, digits=10)
+    biw_p = EigBoundedIW(k_prior.biw.l_σ2, k_prior.biw.u_σ2, νₙ, Ψₙ)
+    Σ = rand(biw_p)
+
+    Σ₀ = inv(τ^2 * I + n * inv(Σ))
+    Σ₀ = round.(Σ₀, digits=10)
+    μ₀ = Σ₀ * (n * inv(Σ) * x̄) |> vec 
+    μ = MvNormal(μ₀, Σ₀) |> rand 
+    return μ, Σ
+end 
